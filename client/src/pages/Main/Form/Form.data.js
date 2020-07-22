39cae925a2e6f97fbb4ecffed0fbefae3312ecd4
Nicklas <nicklascschmidt@nicklass-mbp.lan>
2020-07-22 12:57:28 -0700

    rearranged form data

--- client/src/pages/Main/Form/Form.data.js
+++ client/src/pages/Main/Form/Form.data.js
@@ -5,15 +5,15 @@ export default [
     displayText: 'URL',
   },
   {
+    name: 'numOfTitles',
+    displayText: 'Number of Titles',
+  },
+  {
     name: 'firstTitleText',
     displayText: 'First Title Text',
   },
   {
     name: 'elType',
-    displayText: 'Element Type',
-  },
-  {
-    name: 'numOfTitles',
-    displayText: 'Number of Titles',
+    displayText: 'Element Type (defaults to h2)',
   },
 ];
