5a1cd199386c9cc87395c7189e3c0731a3597a60
Nicklas <nicklascschmidt@nicklass-mbp.lan>
2020-07-22 12:03:16 -0700

    sample preset data for Form

---
+++ client/src/data/sampleMainForm.data.js
@@ -0,0 +1,24 @@
+
+export default [
+  {
+    name: 'Maine',
+    url: 'https://www.thecrazytourist.com/15-best-things-to-do-in-cape-elizabeth-maine/',
+    firstTitleText: '1. Fort Williams Park',
+    elType: 'h2',
+    numOfTitles: '15',
+  },
+  {
+    name: 'Yosemite',
+    url: 'https://www.planetware.com/california/things-to-do-in-yosemite-national-park-us-ca-278.htm',
+    firstTitleText: '1. Yosemite Falls',
+    elType: 'h2',
+    numOfTitles: '13',
+  },
+  {
+    name: 'Washington State',
+    url: 'https://www.tripsavvy.com/top-washington-state-attractions-1608557',
+    firstTitleText: 'Explore Olympic National Park',
+    elType: 'a',
+    numOfTitles: '12',
+  },
+];
