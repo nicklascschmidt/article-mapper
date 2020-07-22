98c7207ad7d7ab65bca9ad587a9ad781f41b5291
Nicklas <nicklascschmidt@nicklass-mbp.lan>
2020-07-22 12:04:12 -0700

    backend routes- added scrape params to accept more data from client

--- routes/html.js
+++ routes/html.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = function(app) {
   app.get('/', (req, res) => {
-    res.send({ test: 'home route' });
+    res.status(200).send({ test: 'home route' });
 
     // res.sendFile(path.join(__dirname, '../','client', 'build', 'index.html'));
   });
--- routes/scrape.js
+++ routes/scrape.js
@@ -1,11 +1,9 @@
 const scrape = require('../scrape/scrape');
 
 module.exports = app => {
-  app.get('/scrape/:url', async (req, res) => {
-    console.log('req.params', req.params);
-
+  app.get('/scrape/:url/:firstTitleText/:elType/:numOfTitles', async (req, res) => {
     const scrapeData = await scrape(req.params);
     
-    res.send(scrapeData);
+    res.status(200).send(scrapeData);
   });
 }
