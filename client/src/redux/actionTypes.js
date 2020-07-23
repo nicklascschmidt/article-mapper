2be4593f87978cb8568726fb9b152774e4a90153
Nicklas <nicklascschmidt@nicklass-mbp.lan>
2020-07-23 10:12:26 -0700

    redux boilerplate code, store setup, integration

---
+++ client/src/redux/actionTypes.js
@@ -0,0 +1,2 @@
+export const ADD_TITLES = 'ADD_TITLES';
+export const REPLACE_TITLES = 'REPLACE_TITLES';
---
+++ client/src/redux/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+import titles from './titles';
+
+/** add reducers within object */
+export default combineReducers({ titles });
