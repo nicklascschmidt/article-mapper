00c03bb2bba21b13f27f0aea1a0be076a85cb885
Nicklas <nicklascschmidt@nicklass-mbp.lan>
2020-07-27 11:04:46 -0700

    added component template to pull from

---
+++ client/src/redux/componentTemplate.js
@@ -0,0 +1,20 @@
+
+
+/**
+ * 
+
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+const mapStateToProps = state => ({
+  titles: state.titles,
+});
+
+const mapDispatchToProps = {};
+
+export default withRouter(connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SearchTitlesForm));
+
+ */
