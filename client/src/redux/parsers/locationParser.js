b4472256532dc3bbfd0cbd9ef157bf3eb78f3bf1
Nicklas <nicklascschmidt@nicklass-mbp.lan>
2020-07-30 19:37:37 -0700

    locations-- separated location parsers to a parser file

--- client/src/redux/parsers/locationParser.js
+++ client/src/redux/parsers/locationParser.js
@@ -4,7 +4,7 @@ import _ from 'lodash';
  * @summary - parses Google Places API location and returns a flat(ish) object
  * @param {object} location - GPlaces API location object
  */
-export const parseLocationBody = (location) => {
+const parseLocationBody = (location) => {
   return {
     _id: _.get(location, '_id', ''),
     userSearchTerm: _.get(location, 'userSearchTerm', ''), // manually passed in
@@ -16,4 +16,28 @@ export const parseLocationBody = (location) => {
     place_id: _.get(location, 'place_id', ''),
     types: _.get(location, 'types', []),
   };
+};
+
+/**
+ * @summary - accepts locations from reducer and returns parsed locations (flat-ish)
+ * @param {object{}} locations - obj of location objs from the reducer
+ * @returns {object{}} - obj of location objs
+*/
+export const parseDeterminedLocations = (locations) => {
+  return Object.keys(locations).reduce((acc, key, idx) => {
+    acc[key] = parseLocationBody(locations[key]);
+    return acc;
+  }, {});
+};
+
+export const parseUndeterminedLocations = (locations) => {
+  return Object.keys(locations).reduce((acc, key, idx) => {
+    const location = locations[key];
+    const parsedLocations = location.possibleLocations.map((location => parseLocationBody(location)));
+    acc[key] = {
+      ...location,
+      possibleLocations: parsedLocations,
+    };
+    return acc;
+  }, {});
 };
