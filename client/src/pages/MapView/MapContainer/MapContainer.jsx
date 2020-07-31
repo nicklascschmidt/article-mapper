ca2d233720c3222965481793afb423fe2607a4ff
Nicklas <nicklascschmidt@nicklass-mbp.lan>
2020-07-30 19:42:47 -0700

    cleaned up MapContainer and moved fetch logic to /thunks

--- client/src/pages/MapView/MapContainer/MapContainer.jsx
+++ client/src/pages/MapView/MapContainer/MapContainer.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
-import axios from 'axios';
 import styled from 'styled-components';
 
 import Map from './Map/Map.jsx';
 
-import { overwriteLocationsField } from '../../../redux/actions';
-import { getDeterminedLocations } from '../../../redux/selectors';
+import { overwriteTitles } from '../../../redux/actions/index';
+import { populateLocationDataFromTitles } from '../../../redux/thunks';
+import { determinedLocationsSelector } from '../../../redux/selectors';
 
 const Container = styled.div``;
 
@@ -27,64 +27,18 @@ class MapContainer extends Component {
   }
 
   componentDidMount = async () => {
-    try {
-      await this.getLocationData();
-    } catch (error) {
-      console.log(error);
-    }
+    const { populateLocationDataFromTitles } = this.props;
+    populateLocationDataFromTitles();
     this.setState({ isLoading: false });
   }
 
-  /**
-   * @summary - Get location data from GPlaces API for all locations
-   *          - Map through locations and add all to a locationData obj of objs
-   *              - determined: single location spread as an obj
-   *              - undetermined: multiple locations in locations.data, rest of fields in obj (status, id, etc.)
-   *          - Update redux with the full obj of locations
-  */
-  getLocationData = async () => {
-    const { titles, overwriteLocationsField } = this.props;
+  componentWillUnmount = () => {
+    const { overwriteTitles, allLocations } = this.props;
 
-    const locationDataPromises = this.getLocationDataPromises();
-    const locations = await Promise.all(locationDataPromises).catch(err => console.log(err));
+    const newTitles = Object.keys(allLocations)
+      .map((key, idx) => allLocations[key].userSearchTerm);
     
-    let locationData = {};
-
-    locations.forEach(async (locationResp, idx) => {
-      const locationArr = _.get(locationResp, 'data.candidates', {});
-
-      const newLocation = (locationArr.length === 1)
-        ? {
-          _id: idx.toString(),
-          status: 'determined',
-          userSearchTerm: titles[idx],
-          ...locationArr[0],
-        } : {
-          _id: idx.toString(),
-          status: 'undetermined',
-          possibleLocations: locationArr,
-        };
-
-      locationData[idx] = newLocation;
-    });
-
-    overwriteLocationsField('data', locationData);
-  }
-
-  getLocationDataPromises = () => {
-    const { titles } = this.props;
-    return titles.map(async (title, idx) => {
-      return this.fetchLocationData(title, { padding: [20, 20] });
-    });
-  }
-
-  fetchLocationData = (searchTerm) => {
-    const params = { fields: 'formatted_address,type' }
-    return axios.get(`/api/client/find-place-from-text/${searchTerm}`, { params });
-  }
-
-  determineCorrectLocation = () => {
-    
+    overwriteTitles(newTitles);
   }
 
   render() {
@@ -104,12 +58,14 @@ class MapContainer extends Component {
 }
 
 const mapStateToProps = state => ({
-  determinedLocations: getDeterminedLocations(state.locations.data),
+  allLocations: state.locations.data,
+  determinedLocations: determinedLocationsSelector(state),
   titles: state.titles.titleStrings,
 });
 
 const mapDispatchToProps = {
-  overwriteLocationsField,
+  overwriteTitles,
+  populateLocationDataFromTitles,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(MapContainer);
