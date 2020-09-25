841b9ad0fe5702f31a64b3a6e15cb9968420e91d
Nicklas <nicklascschmidt@nicklass-mbp.lan>
2020-09-25 10:47:49 -0700

    added sample reducer data for testing

---
+++ client/src/redux/reducers/sampleLocationsData.js
@@ -0,0 +1,346 @@
+export default {
+  '0': {
+    _id: '0',
+    userSearchTerm: 'Lower Yosemite Falls',
+    status: 'determined',
+    formatted_address: 'Lower Yosemite Fall Trail, California 95389, United States',
+    geometry: {
+      location: {
+        lat: 37.7464114,
+        lng: -119.5962486
+      },
+      viewport: {
+        northeast: {
+          lat: 37.74727062989272,
+          lng: -119.5945033701073
+        },
+        southwest: {
+          lat: 37.74457097010727,
+          lng: -119.5972030298927
+        }
+      }
+    },
+    name: 'Lower Yosemite Falls Trailhead',
+    photos: [
+      {
+        height: 3714,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/107923647101936741752">lone ferdinandsen.dk</a>'
+        ],
+        photo_reference: 'CmRaAAAAewhreaqg3JoPE3vHaOuyQ93UVIbf9wOeydStfoVkTUi5edzQeoOwMAciyQbW_ZF6AJv03Okcgkkjs568UO2GgHd4rwE8f2jAPpj5mb1WiOBGKgRfWycAANM2zuJNu4xvEhDZGkWvOW8jg2qUSMARfYMlGhTrMt_REhyxy2mqbHwnEvMRRrbIHA',
+        width: 2444
+      }
+    ],
+    place_id: 'ChIJj8hOGQLyloARdq1zibGmGg4',
+    types: [
+      'tourist_attraction',
+      'park',
+      'point_of_interest',
+      'establishment'
+    ]
+  },
+  '1': {
+    _id: '1',
+    userSearchTerm: 'Mariposa Grove',
+    status: 'determined',
+    formatted_address: 'Mariposa Grove, California 93623, USA',
+    geometry: {
+      location: {
+        lat: 37.5098372,
+        lng: -119.6046848
+      },
+      viewport: {
+        northeast: {
+          lat: 37.52071979999999,
+          lng: -119.5894407
+        },
+        southwest: {
+          lat: 37.4997465,
+          lng: -119.618983
+        }
+      }
+    },
+    name: 'Mariposa Grove',
+    photos: [
+      {
+        height: 7862,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/112936164268807047067">Dexter Ramos</a>'
+        ],
+        photo_reference: 'CmRaAAAAwyuEmH_96e3vpoKTu0EoHuHf7eSpm0kzn-2BeYKVcnaIZL6HulT61FuPNfUGozDtbjzgH3HYvP5biECjIwq8hTGauD-XaatRS7VHFtGhjdlpBn4v9-xCzg3ip1X8CtQWEhAmrUQugOkS6nq0xbmhQ_VIGhRk89NbSMLLFrGEwr5_kXPYwWRerg',
+        width: 3814
+      }
+    ],
+    place_id: 'ChIJ-8YS5wGQloARFRF6x4FRlHI',
+    types: [
+      'natural_feature',
+      'establishment'
+    ]
+  },
+  '2': {
+    _id: '2',
+    userSearchTerm: 'Taft Point and Sentinel Dome via Pohono Trail',
+    status: 'determined',
+    formatted_address: 'Pohono Trail, California, USA',
+    geometry: {
+      location: {
+        lat: 37.69732,
+        lng: -119.6459407
+      },
+      viewport: {
+        northeast: {
+          lat: 37.69866982989272,
+          lng: -119.6445908701073
+        },
+        southwest: {
+          lat: 37.69597017010727,
+          lng: -119.6472905298927
+        }
+      }
+    },
+    name: 'Pohono Trail',
+    place_id: 'ChIJWbnHeK_tloARTRd88FRBwMk',
+    types: [
+      'route'
+    ]
+  },
+  '3': {
+    _id: '3',
+    userSearchTerm: 'The Mist Trail to Vernal and Nevada Falls',
+    status: 'undetermined',
+    possibleLocations: []
+  },
+  '4': {
+    _id: '4',
+    userSearchTerm: 'Cathedral Lakes',
+    status: 'determined',
+    formatted_address: 'Cathedral Lakes, California, USA',
+    geometry: {
+      location: {
+        lat: 37.843523,
+        lng: -119.4209251
+      },
+      viewport: {
+        northeast: {
+          lat: 37.84499862989273,
+          lng: -119.4196033701073
+        },
+        southwest: {
+          lat: 37.84229897010729,
+          lng: -119.4223030298927
+        }
+      }
+    },
+    name: 'Cathedral Lakes',
+    photos: [
+      {
+        height: 2736,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/101711419192995678171">Cynthia Carrier</a>'
+        ],
+        photo_reference: 'CmRaAAAApFHv_AA1RFNSH5EMD9gqgijoB0-Z4nMmJ_EQBM98cMYROeKkPX5dEWgSTtjFWbsa7gYibJDGUDyuYS4YIQUscDFMqYMDt0pzRHzn5KrChtfBshp3_u1_aAipKLjqgy7oEhD1WhTGQLjztYL82RO0RrE0GhRRFHwndQ28uepCZvF1Hpp7kBSmqg',
+        width: 3648
+      }
+    ],
+    place_id: 'ChIJBXU9gtf3loARcvwibTprtcY',
+    types: [
+      'natural_feature',
+      'establishment'
+    ]
+  },
+  '5': {
+    _id: '5',
+    userSearchTerm: 'Panorama Trail',
+    status: 'determined',
+    formatted_address: 'Glacier Point Rd, Yosemite Valley, CA 95389, United States',
+    geometry: {
+      location: {
+        lat: 37.7302595,
+        lng: -119.5737157
+      },
+      viewport: {
+        northeast: {
+          lat: 37.73160314999999,
+          lng: -119.5726609701073
+        },
+        southwest: {
+          lat: 37.72622855,
+          lng: -119.5753606298927
+        }
+      }
+    },
+    name: 'Glacier Point',
+    photos: [
+      {
+        height: 3265,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/116637880737538951708">Cree Putney</a>'
+        ],
+        photo_reference: 'CmRaAAAAoOPRNqKo3kPFy4bsG-W8VIWyfOkdMRprLodGlA3xnd6dyqzaG36UaA8Pc2Z2Q6dVeswolZyEfPbiZ49vNSMvP03xvO2lY7as0FPvwsXCv7RATDChl-7yfAa793Kk4iaDEhDG0efMKz57BwS4umShRz0bGhSCOMvlNEA_m8AP3-zKpvklEr2wUA',
+        width: 4898
+      }
+    ],
+    place_id: 'ChIJid_rw0PyloARCiimK8Lg11g',
+    types: [
+      'tourist_attraction',
+      'point_of_interest',
+      'establishment'
+    ]
+  },
+  '6': {
+    _id: '6',
+    userSearchTerm: 'Glacier Point via Four-Mile Trail',
+    status: 'undetermined',
+    possibleLocations: [
+      {
+        formatted_address: 'Glacier Point Rd, Yosemite Valley, CA 95389, United States',
+        geometry: {
+          location: {
+            lat: 37.7302595,
+            lng: -119.5737157
+          },
+          viewport: {
+            northeast: {
+              lat: 37.73160314999999,
+              lng: -119.5726609701073
+            },
+            southwest: {
+              lat: 37.72622855,
+              lng: -119.5753606298927
+            }
+          }
+        },
+        name: 'Glacier Point',
+        photos: [
+          {
+            height: 3265,
+            html_attributions: [
+              '<a href="https://maps.google.com/maps/contrib/116637880737538951708">Cree Putney</a>'
+            ],
+            photo_reference: 'CmRaAAAADsqMWSsjdnGS6O80NF_ERV27Bq0sVotCUp4JVG_VeZ7mD2QVYNb4RqLEW8HQGd9Ij0INH7fpOE2EUv4OegYENl4u7vyDrjJxHMLu_UMQsV_9PHsX_IAjOKSKskxFAhSJEhCw9PXBbiCRJrq3IvhbdB_jGhRRct3H2oMeK4vXlzQnxFM7PWrzwg',
+            width: 4898
+          }
+        ],
+        place_id: 'ChIJid_rw0PyloARCiimK8Lg11g',
+        types: [
+          'tourist_attraction',
+          'point_of_interest',
+          'establishment'
+        ]
+      },
+      {
+        formatted_address: 'Southside Dr, Yosemite Village, CA 95389, United States',
+        geometry: {
+          location: {
+            lat: 37.7338083,
+            lng: -119.6017656
+          },
+          viewport: {
+            northeast: {
+              lat: 37.73520257989272,
+              lng: -119.6004739201073
+            },
+            southwest: {
+              lat: 37.73250292010728,
+              lng: -119.6031735798927
+            }
+          }
+        },
+        name: 'Four Mile Trailhead',
+        photos: [
+          {
+            height: 800,
+            html_attributions: [
+              '<a href="https://maps.google.com/maps/contrib/114229315108843263351">Sonny Gonzales</a>'
+            ],
+            photo_reference: 'CmRaAAAATlMkBUwJyCB46yvc0ZUbG5EwGNtC7yMBfWucS1tg8lqtU8vDW2nrYX-u4-PmQbeHvezf0UL0zVGxeI3v-TgtURifqymp7McdVkxOHHNS95fUX9mr-1Pu6jK35DOU3Z0WEhBgPb5I3TQUTc-zXFOkqw3bGhRhMwuR60UKIbQSMoDo76zYW3mWaQ',
+            width: 1200
+          }
+        ],
+        place_id: 'ChIJnWRDSQjyloARwrgZjUKroMA',
+        types: [
+          'tourist_attraction',
+          'park',
+          'point_of_interest',
+          'establishment'
+        ]
+      }
+    ]
+  },
+  '7': {
+    _id: '7',
+    userSearchTerm: 'Upper Yosemite Falls',
+    status: 'determined',
+    formatted_address: 'Upper Yosemite Falls, California 95389, United States',
+    geometry: {
+      location: {
+        lat: 37.7568139,
+        lng: -119.5969114
+      },
+      viewport: {
+        northeast: {
+          lat: 37.7656351,
+          lng: -119.580904
+        },
+        southwest: {
+          lat: 37.7479917,
+          lng: -119.6129188
+        }
+      }
+    },
+    name: 'Upper Yosemite Falls',
+    photos: [
+      {
+        height: 9529,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/116257116504825799637">brianfriskyo</a>'
+        ],
+        photo_reference: 'CmRaAAAAe3diDBif3CvROweVr5OYT1WDoSSil_qNj52WzEJX48nqxg_Ii4RueYT5wes93MKOYGqRCtJwwTtO0-Z85bT7Xfm37S8_j7A_Ke30a6cz-h48UK97aYD8b02bbVAxYEjlEhDxg1UU6C2bJBV6WZiHBu6rGhTFf8UGF4Nf2orBEsyOn7iNKhmBhg',
+        width: 6000
+      }
+    ],
+    place_id: 'ChIJ8w-LTvvxloAR897UEV791UI',
+    types: [
+      'natural_feature',
+      'establishment'
+    ]
+  },
+  '8': {
+    _id: '8',
+    userSearchTerm: 'Half Dome via Mist Trail',
+    status: 'determined',
+    formatted_address: 'Half Dome, California, United States',
+    geometry: {
+      location: {
+        lat: 37.7459192,
+        lng: -119.5331992
+      },
+      viewport: {
+        northeast: {
+          lat: 37.7547417,
+          lng: -119.5171918
+        },
+        southwest: {
+          lat: 37.7370957,
+          lng: -119.5492066
+        }
+      }
+    },
+    name: 'Half Dome',
+    photos: [
+      {
+        height: 1149,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/105571470232661846943">Detlef Deiters</a>'
+        ],
+        photo_reference: 'CmRaAAAACpNBpXyCFSxS8JWVLsBNYugBxeFRd4j_USqy1MtXG0bM4mQkI9IILhC9hP6Jsyi4WUHrhB_GobiIxvZq2RZV2G3RO0-B3SBkHjAn82nMBlw9MdtEIWjVBimXpPU0i0zdEhA2mOA-jkc6j9icOsYXjFX5GhQdMtiwRrFZO8_VNtlpCtJzqZSsiw',
+        width: 2048
+      }
+    ],
+    place_id: 'ChIJo8CxverzloARZHrswixEpaM',
+    types: [
+      'natural_feature',
+      'establishment'
+    ]
+  }
+};
---
+++ client/src/redux/reducers/sampleTitlesData.js
@@ -0,0 +1,15 @@
+
+
+export default [
+  'Palette Tea House',
+  'Good Luck Dim Sum',
+  'China Live',
+  'Yank Sing',
+  'MaMa Ji\'s',
+  'Dragon Beaux',
+  'Great Eastern',
+  'Good Mong Kok Bakery',
+  'Chubby Noodle',
+  'Hakkasan',
+  'Hong Kong Lounge II'
+];
