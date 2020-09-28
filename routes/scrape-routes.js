a769a54dd800e272c3583f1f9ad0e5c8eacb3f25
Nicklas <nicklascschmidt@nicklass-mbp.lan>
2020-09-28 16:42:55 -0700

    added error handling note to scrape-routes

--- routes/scrape-routes.js
+++ routes/scrape-routes.js
@@ -2,8 +2,18 @@ const scrape = require('../scrape/scrape');
 
 module.exports = app => {
   app.get('/scrape', async (req, res) => {
-    const scrapeData = await scrape(req.query);
-    
+    let scrapeData;
+    try {
+      scrapeData = await scrape(req.query);
+    } catch (err) {
+      console.log(err);
+      /**
+       * This likely will not hit in bc async awaited call
+       * TODO: impl domains https://nodejs.org/api/domain.html
+       */
+      return res.status(500).send(err.message);
+    }
+
     res.status(200).send(scrapeData);
   });
 }
