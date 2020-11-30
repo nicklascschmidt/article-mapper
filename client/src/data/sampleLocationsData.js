80fbeabbc8770a59a04205bf6727460592168a6c
Nicklas <nicklascschmidt@Nicklass-MacBook-Pro.local>
2020-11-30 16:24:40 -0500

    updated sample titles and locations to match, moved to /data folder

---
+++ client/src/data/sampleLocationsData.js
@@ -0,0 +1,486 @@
+export default {
+  '0': {
+    _id: '0',
+    userSearchTerm: 'Palette Tea House',
+    status: 'determined',
+    formatted_address: '900 North Point St Ste B201, San Francisco, CA 94109, United States',
+    geometry: {
+      location: {
+        lat: 37.8056739,
+        lng: -122.4223763
+      },
+      viewport: {
+        northeast: {
+          lat: 37.80692152989272,
+          lng: -122.4210055201073
+        },
+        southwest: {
+          lat: 37.80422187010728,
+          lng: -122.4237051798927
+        }
+      }
+    },
+    name: 'Palette Tea House & Dim Sum 彩籠',
+    photos: [
+      {
+        height: 1536,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/106655160762588260996">A Google User</a>'
+        ],
+        photo_reference: 'ATtYBwLIImR6Nx7w8BqIGGNkGZOaoDdAVRTJ92eJSOSLV4KIGbQk9xSJcOucArlfTNKZdnV41hz3bA6X3UP524KGwjaI-d0vPpVXZ18RmlYDiGt1hDT7VbjLwbCg_iaAzMHFc1PD48zKviW35-Dc5PJm4udoZ2F5EKxUvPsBSMz3NNfPchZi',
+        width: 2048
+      }
+    ],
+    place_id: 'ChIJ53HCdjCBhYARI-_VU7jZI-U',
+    types: [
+      'restaurant',
+      'food',
+      'point_of_interest',
+      'establishment'
+    ]
+  },
+  '1': {
+    _id: '1',
+    userSearchTerm: 'Good Luck Dim Sum',
+    status: 'determined',
+    formatted_address: '736 Clement St, San Francisco, CA 94118, United States',
+    geometry: {
+      location: {
+        lat: 37.7829939,
+        lng: -122.4672055
+      },
+      viewport: {
+        northeast: {
+          lat: 37.78425867989272,
+          lng: -122.4658396201072
+        },
+        southwest: {
+          lat: 37.78155902010728,
+          lng: -122.4685392798927
+        }
+      }
+    },
+    name: 'Good Luck Dim Sum',
+    photos: [
+      {
+        height: 689,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/103918941748163539279">Krystyna Maliniak</a>'
+        ],
+        photo_reference: 'ATtYBwKZalRueHefTwRLjTn_5tGCdeO4r3OvYRC6H-1Dm8f28W7dMxWcNfti8jDUto1ZVARbKQeWyrxlU3JZtQUog_HtWgH-GiNHXFwvMYocdKE5jfdWZRG7jWqOz-opDVAjoX-8sAhxEBSrqpsf3F0kNi0wBBmfJt4sKyraHhKzgdDxLtuD',
+        width: 918
+      }
+    ],
+    place_id: 'ChIJXWARsD2HhYARpfJUcT_imWw',
+    types: [
+      'restaurant',
+      'food',
+      'point_of_interest',
+      'establishment'
+    ]
+  },
+  '2': {
+    _id: '2',
+    userSearchTerm: 'China Live',
+    status: 'determined',
+    formatted_address: '644 Broadway, San Francisco, CA 94133, United States',
+    geometry: {
+      location: {
+        lat: 37.7978556,
+        lng: -122.4078833
+      },
+      viewport: {
+        northeast: {
+          lat: 37.79915932989272,
+          lng: -122.4064624701073
+        },
+        southwest: {
+          lat: 37.79645967010728,
+          lng: -122.4091621298927
+        }
+      }
+    },
+    name: 'China Live',
+    photos: [
+      {
+        height: 4195,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/112768757465006801865">China Live</a>'
+        ],
+        photo_reference: 'ATtYBwIiTYq7OlO9675-f_ZsLgjVmXoyc3Sr6KGWIthukF8lWb0drelfTGeebEr2pVMyjH34dLzip8GDMD4TmuDOwJJsfbsuL9Nqu2ciZpWvqL3X6U86naeqnHBHTT0w2XsUbiLQ8B0e82OFvj60RQw3-cQ9_LNLR0kkqWW13J8zn93dpHE7',
+        width: 6292
+      }
+    ],
+    place_id: 'ChIJx1IXkvOAhYARMNdQA5IcmEc',
+    types: [
+      'bar',
+      'restaurant',
+      'food',
+      'point_of_interest',
+      'establishment'
+    ]
+  },
+  '3': {
+    _id: '3',
+    userSearchTerm: 'Yank Sing',
+    status: 'undetermined',
+    possibleLocations: [
+      {
+        formatted_address: '101 Spear St, San Francisco, CA 94105, United States',
+        geometry: {
+          location: {
+            lat: 37.7925889,
+            lng: -122.3929775
+          },
+          viewport: {
+            northeast: {
+              lat: 37.79355562989272,
+              lng: -122.3915614
+            },
+            southwest: {
+              lat: 37.79085597010728,
+              lng: -122.3945502
+            }
+          }
+        },
+        name: 'Yank Sing (Rincon)',
+        photos: [
+          {
+            height: 3152,
+            html_attributions: [
+              '<a href="https://maps.google.com/maps/contrib/109306044835103039453">Yank Sing Banquet &amp; Catering</a>'
+            ],
+            photo_reference: 'ATtYBwKKUoW31TSfIcyOviiksKEMfkva0fOZ8FG_ZOeVdo_GvVY3PLFVMo9VDu0EAagJaK6xaDz4SOY94NyDaqFaZJWWhNgPNcN8LJK6jUaKhgRn6eTH5aohodQIdZ2fD1yuqylo9YW1hkqt6-Neu0pC1GZZ1ao6_oVOkMMBEux_pcW-rRA2',
+            width: 4928
+          }
+        ],
+        place_id: 'ChIJE_x2VGSAhYARoXDSoUvUjkI',
+        types: [
+          'restaurant',
+          'food',
+          'point_of_interest',
+          'establishment'
+        ]
+      },
+      {
+        formatted_address: '49 Stevenson St, San Francisco, CA 94105, United States',
+        geometry: {
+          location: {
+            lat: 37.7898659,
+            lng: -122.3993287
+          },
+          viewport: {
+            northeast: {
+              lat: 37.79124727989272,
+              lng: -122.3979584201073
+            },
+            southwest: {
+              lat: 37.78854762010728,
+              lng: -122.4006580798927
+            }
+          }
+        },
+        name: 'Yank Sing (Stevenson St.)',
+        photos: [
+          {
+            height: 3024,
+            html_attributions: [
+              '<a href="https://maps.google.com/maps/contrib/115514178443082364418">yulin niu</a>'
+            ],
+            photo_reference: 'ATtYBwKMvHe7UWWOXB8KMfB525OuvRG8D5MOaiY80ScLwfDk9c9qKV8_fPAfGixVNMCmu1IInRBGU0utoyZt15JWv4kbi3-la4YDwbVpyL0YEK5vJgm0wLdGfazYHEEN6N-bKOdAHejV6_v6Y7qdUnIrae_yY4vjtp85wTkjKJ6rxLgnke_q',
+            width: 4032
+          }
+        ],
+        place_id: 'ChIJ1aUl92KAhYARTY_D5x-dTnE',
+        types: [
+          'restaurant',
+          'food',
+          'point_of_interest',
+          'establishment'
+        ]
+      }
+    ]
+  },
+  '4': {
+    _id: '4',
+    userSearchTerm: 'MaMa Ji\'s',
+    status: 'determined',
+    formatted_address: '4416 18th St, San Francisco, CA 94114, United States',
+    geometry: {
+      location: {
+        lat: 37.7607671,
+        lng: -122.4387173
+      },
+      viewport: {
+        northeast: {
+          lat: 37.76207077989272,
+          lng: -122.4373528701073
+        },
+        southwest: {
+          lat: 37.75937112010728,
+          lng: -122.4400525298927
+        }
+      }
+    },
+    name: 'Mama Ji\'s',
+    photos: [
+      {
+        height: 3024,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/109887258970492724755">Bernard Jong</a>'
+        ],
+        photo_reference: 'ATtYBwLRNBFnRqGvKoGtW1H6NrUIwuXtkx3_4jCyLecjz4YkpQwgMmmQ6KxeNG165LwmWeRvrBMtmvJv4E12wvj9dQ9U_lOreEY5FoFmYCh1PguL9WKTMhsTv8tTAo5_D8U4j6VzZsFePu_uREABq2tWISP5sU-C9wP76uw-0YvTMjvCGJ40',
+        width: 4032
+      }
+    ],
+    place_id: 'ChIJk5c5ZQR-j4ARhHCzKVyTxTc',
+    types: [
+      'restaurant',
+      'food',
+      'point_of_interest',
+      'establishment'
+    ]
+  },
+  '5': {
+    _id: '5',
+    userSearchTerm: 'Dragon Beaux',
+    status: 'determined',
+    formatted_address: '5700 Geary Blvd, San Francisco, CA 94121, United States',
+    geometry: {
+      location: {
+        lat: 37.780508,
+        lng: -122.4806996
+      },
+      viewport: {
+        northeast: {
+          lat: 37.78183672989271,
+          lng: -122.4792431701073
+        },
+        southwest: {
+          lat: 37.77913707010727,
+          lng: -122.4819428298927
+        }
+      }
+    },
+    name: 'Dragon Beaux',
+    photos: [
+      {
+        height: 3024,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/109673791694826464177">Jackie Gordon Singing Chef</a>'
+        ],
+        photo_reference: 'ATtYBwKMYxkDGcQJ5SL8tWvXAQFh31nl4AG7Rp5DMnEIJyiX92I-EYMZdfdQyCBumGeJ0-A8c_KuFYMnLVwaFUqj4DV90RsOY4OYr7LzPCwqQFV7aBjFDo6sI7e9ZDnnui6sH3xchAF2om4l3IAW4od0B1fwdhuQ3Vlses9cruQr3wEGPJg',
+        width: 4032
+      }
+    ],
+    place_id: 'ChIJXbSH3w-HhYAR7x1xc3I2GGA',
+    types: [
+      'restaurant',
+      'food',
+      'point_of_interest',
+      'establishment'
+    ]
+  },
+  '6': {
+    _id: '6',
+    userSearchTerm: 'Great Eastern',
+    status: 'determined',
+    formatted_address: '649 Jackson St, San Francisco, CA 94133, United States',
+    geometry: {
+      location: {
+        lat: 37.79599779999999,
+        lng: -122.4059543
+      },
+      viewport: {
+        northeast: {
+          lat: 37.79740682989272,
+          lng: -122.4046244701073
+        },
+        southwest: {
+          lat: 37.79470717010727,
+          lng: -122.4073241298927
+        }
+      }
+    },
+    name: 'Great eastern restaurant',
+    photos: [
+      {
+        height: 3480,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/111959832032166421404">Y Xu</a>'
+        ],
+        photo_reference: 'ATtYBwLTQMAiVPfZk6kb5tp_RuYfHNli5hOyDku1knS8yrcs15EEXBapxldclxrs6fS9fFO2Nmn6tY2qFsUo3RRG7SrjRDJx0_gFSnVhvx4uZX9ZH8D35AuYzk-nXfV-NxFW8WjRR5DdqTUmBYSUajcojGiiA3dc0QDU_rAVLkPeagva6V0i',
+        width: 4640
+      }
+    ],
+    place_id: 'ChIJPe15ufSAhYARf6hhhS-Kq7w',
+    types: [
+      'restaurant',
+      'food',
+      'point_of_interest',
+      'establishment'
+    ]
+  },
+  '7': {
+    _id: '7',
+    userSearchTerm: 'Good Mong Kok Bakery',
+    status: 'determined',
+    formatted_address: '1039 Stockton St, San Francisco, CA 94108, United States',
+    geometry: {
+      location: {
+        lat: 37.795455,
+        lng: -122.408356
+      },
+      viewport: {
+        northeast: {
+          lat: 37.79679827989273,
+          lng: -122.4069254701073
+        },
+        southwest: {
+          lat: 37.79409862010728,
+          lng: -122.4096251298927
+        }
+      }
+    },
+    name: 'Good Mong Kok Bakery',
+    photos: [
+      {
+        height: 2438,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/111450706673730952587">Albertino M.</a>'
+        ],
+        photo_reference: 'ATtYBwLiBPzFp1naJtkaaqmlwk6LvDjlJfeeg28VXUW5hBYGSNEgiy_CYXBxArDQxFQJ4ay_CbH-6m9MPnjfr7Ugaw4mfoPQIqtXc7fQAX18bGQPGvOlMDr7JRoUrEOosfUmyRoPPzQvVU7g1rGpCQysFpwEy2f4nCh4I2Ub1PeYyoFUEUQd',
+        width: 1960
+      }
+    ],
+    place_id: 'ChIJverWRvOAhYARVfNtAtDviIc',
+    types: [
+      'restaurant',
+      'food',
+      'point_of_interest',
+      'establishment'
+    ]
+  },
+  '8': {
+    _id: '8',
+    userSearchTerm: 'Chubby Noodle',
+    status: 'determined',
+    formatted_address: '570 Green Street off, Columbus Ave, San Francisco, CA 94133, United States',
+    geometry: {
+      location: {
+        lat: 37.7997609,
+        lng: -122.4085676
+      },
+      viewport: {
+        northeast: {
+          lat: 37.80101847989271,
+          lng: -122.4071821701073
+        },
+        southwest: {
+          lat: 37.79831882010727,
+          lng: -122.4098818298927
+        }
+      }
+    },
+    name: 'Chubby Noodle North Beach',
+    photos: [
+      {
+        height: 2448,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/107826664770342464609">Edwina Duenas</a>'
+        ],
+        photo_reference: 'ATtYBwLoesQfk5aY64HEhcw8rNuYloDj9PzSu3df9AtnH0bkiY2nOqFD-AxTtD4ULNIW9UEZbFbRrcJSC9aL9TKp7MPmC9hPVElrQozL9uj4stL2s49q9m4vQrz6oEc9oddWHjHCMGLciT7E_f2KnSAp8qbFPJE7iBAxzHkiTXGxP1Pc2dhE',
+        width: 3264
+      }
+    ],
+    place_id: 'ChIJH-6g9_OAhYARff--WPUO_2w',
+    types: [
+      'meal_delivery',
+      'restaurant',
+      'food',
+      'point_of_interest',
+      'establishment'
+    ]
+  },
+  '9': {
+    _id: '9',
+    userSearchTerm: 'Hakkasan',
+    status: 'determined',
+    formatted_address: '1 Kearny St, San Francisco, CA 94108, United States',
+    geometry: {
+      location: {
+        lat: 37.7878328,
+        lng: -122.4036245
+      },
+      viewport: {
+        northeast: {
+          lat: 37.78924087989272,
+          lng: -122.4023203701073
+        },
+        southwest: {
+          lat: 37.78654122010728,
+          lng: -122.4050200298927
+        }
+      }
+    },
+    name: 'Hakkasan San Francisco',
+    photos: [
+      {
+        height: 6656,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/104930151136628361851">A Google User</a>'
+        ],
+        photo_reference: 'ATtYBwKLxXAPLxfgrEGbM6XilTJlfD-VE3ltLKny6JEejIxD8-DEWfIfDhAd49lWd__rS6xJYPer1SFunWvW5rvbT1uFTXpOPvdl4gCnaX2iVHNQS_Ce6hjZml2-n59EV3NshaXvRMYmdY-EGDtqUXCiR4S2Ii4MoXQs2H7KhVIKdzAJ93ne',
+        width: 13312
+      }
+    ],
+    place_id: 'ChIJZR1IXIiAhYARVdRHncWNWRY',
+    types: [
+      'bar',
+      'restaurant',
+      'food',
+      'point_of_interest',
+      'establishment'
+    ]
+  },
+  '10': {
+    _id: '10',
+    userSearchTerm: 'Hong Kong Lounge II',
+    status: 'determined',
+    formatted_address: '3300 Geary Blvd, San Francisco, CA 94118, United States',
+    geometry: {
+      location: {
+        lat: 37.781865,
+        lng: -122.4544116
+      },
+      viewport: {
+        northeast: {
+          lat: 37.78310512989272,
+          lng: -122.4530394701073
+        },
+        southwest: {
+          lat: 37.78040547010728,
+          lng: -122.4557391298927
+        }
+      }
+    },
+    name: 'Hong Kong Lounge II',
+    photos: [
+      {
+        height: 3036,
+        html_attributions: [
+          '<a href="https://maps.google.com/maps/contrib/108367930861399915235">Jake Wasserman</a>'
+        ],
+        photo_reference: 'ATtYBwK2bw--uCCN978RFAfw1yhFgArSc1cK-msrQUAXoctVIv7ZSJF0w5YMtzxOh0zpxqqeGwiIfWtJ4VcH4YKatij-tMo77AnTqe5YbmIoNlZOUij6gElQ5w1TM99gPnMzAvfkMJBPzd3qxXGiqj51tkT73zbU_yG8rQPWjEbaE5mdgfw0',
+        width: 4048
+      }
+    ],
+    place_id: 'ChIJLfBSRjaHhYARbZdIJVlP8Do',
+    types: [
+      'restaurant',
+      'food',
+      'point_of_interest',
+      'establishment'
+    ]
+  }
+};
--- client/src/data/sampleTitlesData.js
+++ client/src/data/sampleTitlesData.js
@@ -1,5 +1,3 @@
-
-
 export default [
   'Palette Tea House',
   'Good Luck Dim Sum',
